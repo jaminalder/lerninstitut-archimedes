dbf7910e40688c0b4f3e5e7ff081a6827539f2cd
jaminalder <benjamin.stalder@gmail.com>
2023-08-29 22:50:36 +0200

    change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 export default {
-  base: process.env.NODE_ENV === 'production' ? '/lerninstitut-archimedes.github.io/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/lerninstitut-archimedes/' : './',
   root: path.resolve(__dirname, 'src'),
   build: {
     outDir: '../dist'
